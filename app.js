62cac6ff687989256dec2ddc701d3f4e16865f0e
Zia Rehman <iamziarehman@gmail.com>
2021-10-07 00:33:13 +0100

    added node version to package.json, fixed dotenv require line

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.user(compression());
+app.use(compression());
 app.use(helmet());
 app.use(express.static(path.join(__dirname, "public")));
 
